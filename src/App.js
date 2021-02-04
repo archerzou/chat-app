93c35617f752680c8ebf54571276768cbef5ba73
Zou <kevin.zou@honeywell.com>
2021-02-04 16:57:09 +0800

    change the format

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ const App = () => {
 			height='100vh'
 			projectID={projectID}
 			userName={localStorage.getItem('username')}
-      		userSecret={localStorage.getItem('password')}
+			userSecret={localStorage.getItem('password')}
 			renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
 			onNewMessage={() => new Audio('https://chat-engine-assets.s3.amazonaws.com/click.mp3').play()}
 		/>
